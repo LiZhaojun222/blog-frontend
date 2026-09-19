1b6d002733a589897da5031a1a51daa8ca44c4d7
LiZhaojun222 <49398861+LiZhaojun222@users.noreply.github.com>
2026-09-19 23:40:20 +0800

    Update request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,9 @@ import axios from 'axios'
 import { message } from 'antd'
 
 const request = axios.create({
-    baseURL: '/api',
+    baseURL: import.meta.env.VITE_API_URL 
+        ? `${import.meta.env.VITE_API_URL}/api`
+        : '/api',
     timeout: 10000
 })
 
